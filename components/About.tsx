2f8ea4f21d5c36d032b65a4a30f8faca42677d1d
ask-t <asktakahashi@go.byuh.edu>
2024-05-01 11:37:41 -1000

    initial commit

---
+++ components/About.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+
+
+const About = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: string) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="https://drive.google.com/uc?export=view&id=1PVtUgmJvOCOHw2C1FCnCLeHN86wbuAri" width={700} height={700} alt={""} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
+            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ components/Skill.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image';
+const Skill = () => {
+  const base: string = "https://drive.google.com/uc?export=view&id=";
+  const languages = [
+    { name: 'Python', icon: `${base}1rvZ0LS5LBeK8BfijW-pkFMkddH0osvMu` },
+    { name: 'C++', icon: `${base}1shHFD451hhre95nftht8YDXxkXffdYe8` },
+    { name: 'C#', icon: `${base}1wTU3l2Ti9VYUva-kK9_lMJ4tcY7stgPl` },
+    { name: 'Java', icon: `${base}1yp44avbIeuH3ENJ6dby36z_3NqhMbkPf` },
+    { name: 'JavaScript', icon: `${base}1v3AhqUM_bxqGv7uvQpCL8F8soE9su7Yk` },
+    { name: 'TypeScript', icon: `${base}1P8wt3C_pwOQDC1j-0XCzEX-MGwHn6_js` },
+    { name: 'Android', icon: `${base}1hrvf166JTW4DuquxE-qhRbIZHsVHFLwC` },
+    { name: 'React', icon: `${base}1jdh1Q5SOMFop9NFHZ-0wuuk29zrFFcgI` },
+    { name: 'Mendix', icon: `${base}11JFi20sazixYP2OZpo6OTyX6iSr8MJsm` },
+    { name: 'ASP.NET', icon: `${base}1xboX3z-cvj6rvgfGeLJBw-G9rONAo0gp` },
+    { name: 'MySQL', icon: `${base}1QeUqPVnrFrJbBxBkmu3XviR_mcRM4Mzl` },
+    { name: 'mongoDB', icon: `${base}11E_Fv8Jg-9J2d082-BjmNrPwxCKi-8z6` },
+    { name: 'Oracle SQL', icon: `${base}1lqOTxWdxhhKc2d9xrkfneSYHACaxF2Wi` },
+    { name: 'Rest API', icon: `${base}15f09sF66AAXcE8j6oNUHBAm1HyoyoGxl` },
+    { name: 'GitHub', icon: `${base}1ukKp8a4yJIs2qngmARoSHd7l6BhpLCsj` },
+  ];
+
+  return (
+    <div className='container'>
+      <h2 className="text-4xl font-bold text-white mb-4 pb-10">Skill</h2>
+      <div className="grid grid-cols-3 gap-10 sm:grid-cols-2 md:grid-cols-5">
+        {languages.map(lang => (
+          <div key={lang.name} className="flex flex-col items-center ">
+            <Image src={lang.icon} alt={`${lang.name} icon`} width={200} height={200} />
+            <span className="mt-2 text-2xl text-center text-white">{lang.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skill

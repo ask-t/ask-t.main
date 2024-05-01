6ff595b5b987300bf3251bc8e6ad7fff1a702097
ask-t <asktakahashi@go.byuh.edu>
2024-05-01 11:37:56 -1000

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <p className="text-slate-600 text-center m-6">All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
